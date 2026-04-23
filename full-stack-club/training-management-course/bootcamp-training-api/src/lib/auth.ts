69c6f1a00a5986065c67198cdd9e8b3af1072474
rphlrz <826.xxxxx@gmail.com>
2026-04-23 01:43:13 -0300

    🔧 chore(auth): update production cookie domain

--- full-stack-club/training-management-course/bootcamp-training-api/src/lib/auth.ts
+++ full-stack-club/training-management-course/bootcamp-training-api/src/lib/auth.ts
@@ -23,7 +23,7 @@ export const auth = betterAuth({
     crossSubDomainCookies: {
       enabled: true,
       domain:
-        env.NODE_ENV === "production" ? ".fullstackclub.com.br" : undefined,
+        env.NODE_ENV === "production" ? ".rlzcode.com" : undefined,
     },
   },
 });
